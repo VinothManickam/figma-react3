17d289ef6d939277d2683b9b8abd601fc0c09285
VinothManickam <103953710+VinothManickam@users.noreply.github.com>
2023-07-17 20:49:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# figma-react3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-phrad7)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./style.css";
+const Desktop3 = () => {
+  return (
+    <div className={styles.desktop3}>
+      <div className={styles.findYourDream}>Find your dream job now</div>
+      <div className={styles.group239variant4}>
+        <div className={styles.group239variant4Child} />
+        <img
+          className={styles.basilsearchOutlineIcon}
+          alt=""
+          src="/basilsearchoutline.svg"
+        />
+        <div className={styles.search}>Search...</div>
+      </div>
+    </div>
+  );
+};
+
+export default Desktop3;
